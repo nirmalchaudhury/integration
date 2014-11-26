45ccbedeefd0f9d5f043cbf2bb9debd791e85bcb
Nirmal Chaudhury <nchaudhury@gmail.com>
2014-11-25 21:14:45 -0500

    Added exclamation mark

--- playground/java/hello-world/src/main/java/main.java
+++ playground/java/hello-world/src/main/java/main.java
@@ -9,11 +9,11 @@ public class main {
 	 */
 	public static void main(String[] names) {
 		if (names.length == 0) {
-			System.out.println("Hello World");
+			System.out.println("Hello World!");
 		}
 		else {
 			for(String name : names) {
-				System.out.println("Hello " + name);
+				System.out.println("Hello " + name + "!");
 			}
 		}		
 	}
